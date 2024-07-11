0b65a1b7e3b02f065e681e390e9e4769853b56f2
joaomaxboico <joo_max@yahoo.com.br>
2024-07-11 20:43:52 -0300

    Primeiro commit

---
+++ Networks/5_COMPOSE/4_projeto/mysql/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS flaskdocker;
+USE flaskdocker;
+
+CREATE DATABASE flaskdocker;
+
+CREATE TABLE 'flaskdocker'.'users' (
+    'id' INT NOT NULL AUTO_INCREMENT,
+    'name' VARCHAR(255),
+    PRIMARY KEY (ID));
